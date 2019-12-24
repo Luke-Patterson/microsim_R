e15ef1e36b78f647de192f8316c629255a0179f9
Luke-Patterson <lsp52@cornell.edu>
2019-12-24 10:09:14 -0500

    update

--- TEST_execution.R
+++ TEST_execution.R
@@ -22,30 +22,35 @@ source("0_master_execution_function.R")
 
 
 # subsequent times, can run from saved r data frames to save time.
-timestart <<- Sys.time()
-d <- policy_simulation(
-                  saveCSV=TRUE,
-                  xvars=c("widowed", "divorced", "separated", "nevermarried", "female", 
-                          'age_cat', "ltHS", "someCol", "BA", "GradSch", "black", 
-                          "other", "asian",'native', "hisp","nochildren",'faminc_cat','coveligd'),
-                  base_bene_level=.55,
-                  impute_method="logit",
-		              makelog = FALSE,
-                  sample_prop=1,
-		              state='WY',
-		              SELFEMP=FALSE,
-		              place_of_work = TRUE,
-		              dual_receiver = .75,
-                  ext_resp_len = TRUE, len_method = 'mean', sens_var = 'resp_len', progalt_post_or_pre ='post',
-		              ext_base_effect=TRUE, extend_prob=.01, extend_days=1, extend_prop=1.01, topoff_rate=.01, topoff_minlength=10,
-                  bene_effect=TRUE, full_particip=FALSE, wait_period=5, clone_factor=1, week_bene_cap=1216,
-                  own_uptake=.25, matdis_uptake=.25, bond_uptake=.25, illparent_uptake=.25,
-                  illspouse_uptake=.25, illchild_uptake=.25,
-                  maxlen_own =260, maxlen_matdis =260, maxlen_bond =30, maxlen_illparent =30, 
-                  maxlen_PFL= 30, maxlen_DI=260, maxlen_total=260,
-                  maxlen_illspouse =30, maxlen_illchild =30,earnings=30000, own_elig_adj= .75,
-                  formula_value_cuts=c(20000, 50000, 100000), formula_bene_levels=c(.4,.5,.6,.7),
-                  output='test_execution', output_stats=c('state_compar'), random_seed=NULL)
+methods <- c('Naive_Bayes','random','KNN1','logit', 'KNN_multi','random_forest','ridge_class','xg_boost')
 
-timeend <<- Sys.time()
-print(timeend - timestart)
+for (meth in methods) {
+  
+  timestart <<- Sys.time()
+  d <- policy_simulation(
+                    saveCSV=TRUE,
+                    xvars=c("widowed", "divorced", "separated", "nevermarried", "female", 
+                            'age_cat', "ltHS", "someCol", "BA", "GradSch", "black", 
+                            "other", "asian",'native', "hisp","nochildren",'faminc_cat','coveligd'),
+                    base_bene_level=.55,
+                    impute_method=meth,
+  		              makelog = FALSE,
+                    sample_prop=1,
+  		              state='WY',
+  		              SELFEMP=FALSE,
+  		              place_of_work = TRUE,
+  		              dual_receiver = .75,
+                    ext_resp_len = TRUE, len_method = 'mean', sens_var = 'resp_len', progalt_post_or_pre ='post',
+  		              ext_base_effect=TRUE, extend_prob=.01, extend_days=1, extend_prop=1.01, topoff_rate=.01, topoff_minlength=10,
+                    bene_effect=TRUE, full_particip=FALSE, wait_period=5, clone_factor=1, week_bene_cap=1216,
+                    own_uptake=.25, matdis_uptake=.25, bond_uptake=.25, illparent_uptake=.25,
+                    illspouse_uptake=.25, illchild_uptake=.25,
+                    maxlen_own =260, maxlen_matdis =260, maxlen_bond =30, maxlen_illparent =30, 
+                    maxlen_PFL= 30, maxlen_DI=260, maxlen_total=260,
+                    maxlen_illspouse =30, maxlen_illchild =30,earnings=30000, own_elig_adj= .75,
+                    formula_value_cuts=c(20000, 50000, 100000), formula_bene_levels=c(.4,.5,.6,.7),
+                    output='test_execution', output_stats=c('state_compar'), random_seed=NULL)
+  
+  timeend <<- Sys.time()
+  print(timeend - timestart)
+}
